Fixed small bugs in connectivity.py, arising when the number of paths is small; added RCL model; added computation and exporting of average thickness; minor changes

core/connectivity.py
@@ -14,7 +14,14 @@ def build_slices(portions, problem_data):
                                                    threshold_length=problem_data.min_stenoses_length)
         else:
             stenoses = find_stenoses(portions, problem_data.stenoses)
-    breakpoints = np.vstack((stenoses, bifurcations)) if stenoses is not None else bifurcations
+    if bifurcations.shape[0] > 0 and stenoses is not None:
+        breakpoints = np.vstack((stenoses, bifurcations))
+    elif bifurcations.shape[0] > 0:
+        breakpoints = bifurcations
+    elif stenoses is not None:
+        breakpoints = stenoses
+    else:
+        breakpoints = np.array(0)
     breakpoints = simplify_bifurcations(breakpoints, problem_data.tol)
 
     for portion in portions:
@@ -116,6 +123,8 @@ def find_bifurcations(portions, tol):
 
 
 def simplify_bifurcations(bifurcations, tol):
+    if len(bifurcations.shape) == 1:
+        bifurcations = np.expand_dims(bifurcations, 0)
     nbifurcations = bifurcations.shape[0]
 
     indices = list(range(nbifurcations))

core/models.py
@@ -126,21 +126,34 @@ class RLC_model(RC_model, RL_model):
         return RL_model.compute_L(self)
 
 
+class RCL_model(RC_model, RL_model):
+    def __init__(self, portions, problem_data):
+        RC_model.__init__(self, portions, problem_data)
+        RL_model.__init__(self, portions, problem_data)
+        return
+
+    def compute_R(self):
+        return RC_model.compute_R(self)
+
+    def compute_C(self):
+        return RC_model.compute_C(self)
+
+    def compute_L(self):
+        return RL_model.compute_L(self)
+
+    def get_matrix_dot(self):
+        return np.array([[0.0, 0.0, 0.0, self.L],
+                         [self.C, 0.0, -self.R * self.C, 0.0]])
+
+
 class Windkessel2(RC_model):
     def __init__(self, portions, problem_data):
         super().__init__(portions, problem_data)
         return
 
     def get_matrix_dot(self):
-        return np.array([[self.C * self.R, -self.C * self.R, 0.0, 0.0],
+        return np.array([[-self.C * self.R, self.C * self.R, 0.0, 0.0],
                          [0.0, 0.0, 0.0, 0.0]])
-
-    def get_matrix(self):
-        return np.array([[-1.0, 1.0, self.R, 0.0],
-                         [0.0, 0.0, 1.0, -1.0]])
-
-    def get_constant(self):
-        return np.array([[0.0, 0.0]]).T
 
 # The constitutive equation is written in this form:
 #
@@ -233,7 +246,6 @@ class YoungTsai(ModelStenosis):
     def compute_R2(self):
         self.vessel_portion.compute_min_radius()
         Kt = 1.52
-        # Kt = 27.3 * 1e-16 * np.exp(34 * self.S) + 0.26 * np.exp(1.51 * self.S)
         self.R2 = float((Kt * self.problem_data.density) /
                         (2 * np.pi ** 2 * self.r0 ** 4) *
                         ((self.r0 / self.vessel_portion.min_radius) ** 2 - 1) ** 2)

core/output_writer.py
@@ -127,14 +127,31 @@ class OutputWriter:
 
     def write_thickess_caps(self):
         outfile = open(self.output_fdr + "/thickness.txt", "w")
+
+        radia_sum = 0
+        arclength_sum = 0
+
+        outfile.write("Data for variable thickness simulations\n")
         for portion in self.portions:
+            portion.compute_mean_radius()
             posindices = np.where(portion.radii > 0)
             posradii = portion.radii[posindices]
+            posarclength = portion.arclength[posindices]
+
+            cur_arclength = posarclength[-1] - posarclength[0]
+            radia_sum += (2 * portion.mean_radius * cur_arclength)
+            arclength_sum += cur_arclength
+
             curstr = portion.pathname + " "
-            # we consider 10% of the radius for the membrane thickness
+            # we consider 10% of the diameter for the membrane thickness
             curstr += str(2 * posradii[0] * 0.1) + " "
             curstr += str(2 * posradii[-1] * 0.1) + "\n"
             outfile.write(curstr)
+
+        avg_thickness = radia_sum / arclength_sum * 0.1
+        outfile.write("\nData for constant thickness simulation\n")
+        outfile.write(f"AVG {avg_thickness}\n")
+
         outfile.close()
 
         return

examples/CoronaryP9/coronary9.py
@@ -7,7 +7,7 @@ from core.connectivity import *
 from core.plot_tools import *
 from core.physical_block import *
 from core.ode_system import ODESystem
-from core.bdf import BDF2
+from core.bdf import *
 from core.bcmanager import BCManager
 from core.rc_calculator import RCCalculator
 from scipy.integrate import simps